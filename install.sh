2e0e2384feea912e743e87c3299deab1e6ce9637
tomyukn <5608467+tomyukn@users.noreply.github.com>
2020-12-19 04:28:04 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,16 +5,6 @@ set -u
 BASEDIR=$(dirname $0)
 cd ${BASEDIR}
 
-# .bin
-if [ ! -d "${HOME}/bin" ]; then
-  mkdir  ${HOME}/bin
-fi
-
-# .config
-if [ ! -d "${HOME}/.config" ]; then
-  mkdir  ${HOME}/.config
-fi
-
 # .vim
 if [ ! -d "${HOME}/.vim" ]; then
   mkdir  ${HOME}/.vim
